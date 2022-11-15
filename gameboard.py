bfb5583ea3367bfd8605576498debb16b498effa
andrewhought <80929614+boolin9@users.noreply.github.com>
2022-11-15 12:08:54 -0700

    Added game board GUI

--- gameboard.py
+++ gameboard.py
@@ -1,6 +1,32 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import pygame as pg
+
+
+BG_COLOR = (155, 213, 175)
+TRIM_COLOR = (216, 245, 231)
+BLACK = (0, 0, 0)
+
+pg.init()
 
 class Gameboard:
-    pass
+    def __init__(self):
+        self.screen = pg.display.set_mode((600, 600))
+        pg.display.set_caption('Tic-tac-toe')
+        self.screen.fill(BG_COLOR)
+        pg.display.flip()
+        self.clock = pg.time.Clock()
+        self.state = True
+        self.draw_lines()
+    
+    
+    def draw_lines(self):
+        lines = [[(200, 0), (200, 600)], 
+                 [(400, 0), (400, 600)],
+                 [(0, 200), (600, 200)],
+                 [(0, 400), (600, 400)]]
+        
+        for item in lines:
+            pg.draw.line(surface=self.screen,
+                        color=TRIM_COLOR,
+                        start_pos=item[0],
+                        end_pos=item[1],
+                        width=1)
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# Initialize game
+from gameboard import Gameboard
+import pygame as pg
+
+
+gb = Gameboard()
+
+
+def main():
+    while gb.state:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                gb.state = False
+                
+        pg.display.update()
+        gb.clock.tick(30)
+
 
 if __name__ == "__main__":
-    pass
+    main()
